ac46587b5c679dda8987171880c1c702b3e751b1
Eduardo Gozalo <tacgozca2024@gmail.com>
2026-06-15 21:16:49 -0500

    pf18

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import WhatsAppButton from './components/WhatsAppButton'
@@ -9,7 +9,7 @@ import Contacto from './pages/Contacto'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,6 +19,6 @@ export default function App() {
       </Routes>
       <Footer />
       <WhatsAppButton />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
