6998371c104e2de145cc8677c525e684a4147007
Eduardo Gozalo <tacgozca2024@gmail.com>
2026-06-17 19:34:56 -0500

    18

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/PowerFlex-Gym/',
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
+    react({
+      babel: {
+        presets: ['babel-preset-react-compiler']
+      }
+    })
   ],
 })
-
-
